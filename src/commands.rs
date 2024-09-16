e6f12a3b2a347e7fdeadf45f701309023e7867de
Alex Gravem <recoeur@proton.me>
2024-09-16 04:16:33 +0100

    feat(commands): implements echo

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,57 @@
+use crate::cursor::Error as RespError;
+use crate::resp::parse;
+use crate::resp::RespValue;
+
 pub enum Command {
     Ping,
+    Echo(String),
+}
+
+impl Command {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, RespError> {
+        let RespValue::Array(arr) = parse(bytes)? else {
+            return Err(RespError::InvalidInput(
+                String::from_utf8(bytes.to_vec()).unwrap(),
+            ));
+        };
+
+        if arr.len() == 0 {
+            return Err(RespError::InvalidInput(
+                String::from_utf8(bytes.to_vec()).unwrap(),
+            ));
+        }
+
+        let RespValue::BulkString(cmd) = arr.get(0).unwrap() else {
+            return Err(RespError::InvalidInput(
+                String::from_utf8(bytes.to_vec()).unwrap(),
+            ));
+        };
+
+        match cmd.to_ascii_uppercase().as_str() {
+            "PING" => Ok(Command::Ping),
+            "ECHO" => {
+                if arr.len() != 2 {
+                    return Err(RespError::InvalidInput(
+                        String::from_utf8(bytes.to_vec()).unwrap(),
+                    ));
+                }
+
+                let RespValue::BulkString(msg) = arr.get(1).unwrap() else {
+                    return Err(RespError::InvalidInput(
+                        String::from_utf8(bytes.to_vec()).unwrap(),
+                    ));
+                };
+
+                Ok(Command::Echo(msg.to_string()))
+            }
+            _ => Err(RespError::InvalidInput(
+                String::from_utf8(bytes.to_vec()).unwrap(),
+            )),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@
 use std::io::{Read, Write};
 
 use anyhow::Context;
+use commands::Command;
+use resp::RespValue;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
 };
 
+mod commands;
+mod cursor;
 mod resp;
 
 #[tokio::main]
@@ -18,8 +22,18 @@ async fn main() -> anyhow::Result<()> {
 
     loop {
         let (mut socket, _addr) = listener.accept().await?;
+
+        async fn send(socket: &mut tokio::net::TcpStream, msg: RespValue) -> anyhow::Result<()> {
+            socket
+                .write_all(&msg.as_bytes())
+                .await
+                .context("Failed to write to socket")?;
+            Ok(())
+        }
+
         tokio::spawn(async move {
             let mut buf = [0; 1024];
+
             loop {
                 let n = socket
                     .read(&mut buf)
@@ -31,13 +45,23 @@ async fn main() -> anyhow::Result<()> {
                     return;
                 }
 
-                tracing::info!("Received: {:?}", &buf[..n]);
-
-                socket
-                    .write_all(b"+PONG\r\n")
-                    .await
-                    .context("Failed to write to socket")
-                    .unwrap();
+                match Command::from_bytes(&buf[..n]) {
+                    Ok(Command::Ping) => {
+                        tracing::info!("Received PING");
+                        let reply = RespValue::SimpleString("PONG".to_string());
+                        send(&mut socket, reply).await.unwrap();
+                    }
+                    Ok(Command::Echo(arg)) => {
+                        tracing::info!("Received ECHO: {:?}", arg);
+                        let reply = RespValue::SimpleString(arg);
+                        send(&mut socket, reply).await.unwrap();
+                    }
+                    Err(e) => {
+                        tracing::warn!("Error: {:?}", e);
+                        let reply = RespValue::Error("unknown command".to_string());
+                        send(&mut socket, reply).await.unwrap();
+                    }
+                }
             }
         });
     }
